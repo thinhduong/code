d9decc0fe8d10b1fa18d9500d127fc239703483a
thinh <shitshit@123>
2017-04-26 06:43:07 +0700

    Red John Is Back

--- src/main/scala/dynamic/RedJohnIsBack.scala
+++ src/main/scala/dynamic/RedJohnIsBack.scala
@@ -2,19 +2,46 @@ package dynamic
 
 object RedJohnIsBack {
   def solve(n: Int): Long = {
-    val arr = Array.fill(n+1)(0L)
+    def go(m: Int): Int = {
+      if (m == 1)
+        0
+      else if (m == 2)
+        1
+      else if (m == 3)
+        2
+      else {
+        var c = 2
+        for (i <- (5 to m)) {
+          var isPrime = true
+          for (j <- (2 to math.sqrt(m).toInt + 1)) {
+            if (i % j == 0)
+              isPrime = false
+          }
 
-    for(i <- (1 to 3)) {
-      arr(i) = 1
+          if (isPrime)
+            c += 1
+        }
+        c
+      }
     }
 
-    arr(4) = 2
+    if (n <= 3)
+      0
+    else {
+      val arr = Array.fill(n + 1)(0)
 
-    for(i <- (5 to n)) {
-      arr(i) = arr(i - 1) + arr(i - 4)
+      for (i <- (1 to 3)) {
+        arr(i) = 1
+      }
+
+      arr(4) = 2
+
+      for (i <- (5 to n)) {
+        arr(i) = arr(i - 1) + arr(i - 4)
+      }
+
+      go(arr(n))
     }
-
-    arr(n)
   }
 
   def main(args: Array[String]): Unit = {
