96542ad31dcb416bd94cb103d8c407ce08399c5d
tduong <thinhduong@sdl.com>
2017-04-28 18:27:05 +0700

    restructure code base

--- src/main/scala/dynamic/NikitaAndTheGame.scala
+++ src/main/scala/dynamic/NikitaAndTheGame.scala
@@ -1,49 +1,33 @@
 package dynamic
 
 object NikitaAndTheGame {
-  def build(n: Int, xs: Seq[Int]): Array[Array[Int]] = {
-    val arr = Array.fill(n, n)(0)
+  def solve(n: Int, xs: Seq[Int]): Int = {
+    val s = Array.fill(n, n)(0)
+    val p = Array.fill(n, n)(0)
 
     for (i <- (0 until n))
-      arr(i)(i) = xs(i)
+      s(i)(i) = xs(i)
 
-    for (i <- (0 until n)){
-      for (j <- (i + 1 until n)) {
-         arr(i)(j) = arr(i)(j - 1) + arr(j)(j)
-      }
-    }
+    for (i <- (1 until n)){
+      for (j <- (0 until n)) {
+        if (j + i < n) {
+          s(j)(j + i) = s(j)(j + i - 1) + s(j + i)(j + i)
 
-    arr
-  }
+          if (s(j)(j + i) % 2 == 1)
+            p(j)(j + i) = 0
+          else {
+            val xs = (0 until i - 1).filter(k => s(j)(k) == s(k+1)(j+i))
 
-  def solve(n: Int, xs: Seq[Int]): Int = {
-    def go(from: Int, to: Int, sum: Int, mem: Array[Array[Int]]): Int = {
-      if (to - from < 1 || sum % 2 == 1)
-        0
-      else {
-        val half = sum / 2
-
-        val idx = mem(from).indexOf(half)
-
-        if (idx == -1)
-          0
-        else {
-          val l = go(from, idx, half, mem)
-          val r = go(idx + 1, to, half, mem)
-
-          math.max(l, r) + 1
+            if (xs.isEmpty)
+              p(j)(j + i) = 0
+            else
+              p(j)(j + i) = xs.map(k => math.min(p(j)(k), p(k+1)(j+i)) + 1).max
+          }
         }
       }
     }
 
-    val sumOfXs = xs.sum
-    if (n < 2 || sumOfXs % 2 == 1)
-      0
-    else {
-      val mem = build(n, xs)
-
-      go(0, n - 1, sumOfXs, mem)
-    }
+    p(0)(n-1)
   }
 
   def main(args: Array[String]): Unit = {
--- src/test/scala/dynamic/NikitaAndTheGameSuite.scala
+++ src/test/scala/dynamic/NikitaAndTheGameSuite.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class NikitaAndTheGameSuite extends FunSuite {
-  /*test("test 1") {
+  test("test 1") {
     val ret = NikitaAndTheGame.solve(7, List(4, 1, 0, 1, 1, 0, 1))
 
     assert(ret === 3)
@@ -22,11 +22,7 @@ class NikitaAndTheGameSuite extends FunSuite {
     val ret = NikitaAndTheGame.solve(4, List(2, 2, 2, 2))
 
     assert(ret === 2)
-  }      */
-
-  test("test 4") {
-    val ret = NikitaAndTheGame.solve(4, List(4, 0, 0, 0))
-
-    assert(ret === 0)
   }
+
+
 }
